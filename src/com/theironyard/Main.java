db23a1d36b21f863cab7f677a414e5e622f16aa7
DrScott <dougscott2@gmail.com>
2015-10-27 13:17:01 -0400

    added an edit and delete post functions

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,52 +1,87 @@
 package com.theironyard;
-
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //ArrayList<User> users = new ArrayList();
-        //int place;
-        //int x = 0;
-        User user = new User();
-        //Post post = new Post();
+        //User user = new User();
         ArrayList<Post> posts = new ArrayList();
-        Spark.staticFileLocation("/public");
-        Spark.post(
-                "/create-account",
+       //Spark.staticFileLocation("/public");
+        //Spark.init();
+        Spark.get(
+                "/",
                 ((request, response) -> {
-                    user.name = request.queryParams("username");
-                    response.redirect("/posts");
+                    Session session = request.session();
+                    String name = session.attribute("username");
+                    if (name == null){
+                        return new ModelAndView(new HashMap(), "not-logged-in.html");
+                    }
+                    HashMap m = new HashMap();
+                    m.put("username", name);
+                    m.put("posts", posts);
+                    return new ModelAndView(m, "/logged-in.html");
+                }),
+                new MustacheTemplateEngine()
+        );//end get
+        Spark.post(
+                "/create-user",
+                ((request, response) -> {
+                    String name = request.queryParams("username");
+                    Session session = request.session();
+                    session.attribute("username", name);
+                    response.redirect("/");
                     return "";
                 })
         );//end post
-         Spark.post(
-                "/create-post",
+        Spark.post(
+                 "/create-post",
+                 ((request, response) -> {
+                     Post post = new Post();
+                     post.id = posts.size() + 1; //haha don't have to make a new int above and ++ it
+                     post.text = request.queryParams("text");
+                     posts.add(post);
+                     response.redirect("/");
+                     return "";
+                 })
+         );//end 2nd post
+        Spark.post(
+                "/delete-post",
                 ((request, response) -> {
-                    Post post = new Post();
-                    post.text = request.queryParams("posts");
-                    posts.add(post);
-                    response.redirect("/posts");
+                    String id = request.queryParams("postid");
+                    try{
+                        int idNum = Integer.valueOf(id);
+                        posts.remove(idNum - 1);
+                        for (int i =0; i<posts.size(); i++){
+                            posts.get(i).id = i + 1;
+                        }
+                    }
+                    catch (Exception e){
+                    }
+                    response.redirect("/");
                     return "";
                 })
-        );//end 2nd post
-        Spark.get(
-                "/posts",
+        );
+        Spark.post(
+                "/edit-post",
                 ((request, response) -> {
-                    HashMap m = new HashMap();
-                    m.put("username", user.name);
-                    m.put("posts", posts);
-                    return new ModelAndView(m, "posts.html");
-                }),
-                new MustacheTemplateEngine()
+                    String id = request.queryParams("postid");
+                    try{
+                        int idNum = Integer.valueOf(id);
+                        posts.get(idNum-1).text = request.queryParams("edit");
+                        for (int i = 0; i<posts.size(); i++){
+                            posts.get(i).id = i+1;
+                        }
+                    }catch (Exception e){
 
-        );//end get
-        System.out.println("");
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
     }
 }
--- src/com/theironyard/User.java
+++
@@ -1,9 +0,0 @@
-package com.theironyard;
-
-/**
- * Created by DrScott on 10/26/15.
- */
-public class User {
-    String name;
-    //String post;
-}
